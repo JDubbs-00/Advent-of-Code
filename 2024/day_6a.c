b6032c082ace1d6644176b735aedd699c9ff9c9b
JDubbs-00 <joeljw00@gmail.com>
2024-12-06 22:52:17 -0700

    Day 6a solution

--- 2024/day_6a.c
+++ 2024/day_6a.c
@@ -1,7 +1,7 @@
 #include "../utl.h"
 
-#define TEST
-//#define REAL
+//#define TEST
+#define REAL
 
 // GUESSES:
 
@@ -10,13 +10,19 @@ int main(int argc, char *argv[])
     clock_t start = clock();
     FILE *fp;
     char *line = _malloc(MAX_LINE_LEN);
-    int res;
+    int res, size, i, j, x, y;
+    direction dir;
+    bool out;
+    char **grid;
+    bool **visited;
 
     #ifdef TEST
     fp = fopen("input/day_6_example_input.txt", "r");
+    size = 10;
     #endif
     #ifdef REAL
     fp = fopen("input/day_6_input.txt", "r");
+    size = 130;
     #endif
     if (fp == NULL)
     {
@@ -24,20 +30,109 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    
-
+    grid = _malloc(size * sizeof(char *));
+    visited = _malloc(size * sizeof(bool *));
+    i = 0;
+    out = false;
+    dir = UP;
     while ((fgets(line, MAX_LINE_LEN, fp)) != NULL)
     {
         clean_input(line);
         //printf("%s", line);
+
+        grid[i] = _malloc(size * sizeof(char));
+        visited[i] = _malloc(size * sizeof(bool));
+        copy_str(line, grid[i], size);
+        j = find_in_str(line, '^');
+        if (j != -1)
+        {
+            x = i;
+            y = j;
+        }
+        i++;
     }
 
-    
+    while (!out)
+    {
+        visited[x][y] = true;
+        switch (dir)
+        {
+            case UP:
+                if (x == 0)
+                {
+                    out = true;
+                }
+                else if (grid[x - 1][y] == '#')
+                {
+                    dir = RIGHT;
+                }
+                else
+                {
+                    x--;
+                }
+                break;
+            case RIGHT:
+                if (y == size - 1)
+                {
+                    out = true;
+                }
+                else if (grid[x][y + 1] == '#')
+                {
+                    dir = DOWN;
+                }
+                else
+                {
+                    y++;
+                }
+                break;
+            case DOWN:
+                if (x == size - 1)
+                {
+                    out = true;
+                }
+                else if (grid[x + 1][y] == '#')
+                {
+                    dir = LEFT;
+                }
+                else
+                {
+                    x++;
+                }
+                break;
+            default: //LEFT
+                if (y == 0)
+                {
+                    out = true;
+                }
+                else if (grid[x][y - 1] == '#')
+                {
+                    dir = UP;
+                }
+                else
+                {
+                    y--;
+                }
+                break;
+        }
+
+    }
+
+    res = 0;
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            if (visited[i][j])
+            {
+                res++;
+            }
+        }
+    }
 
     if (ferror(fp)) printf("Error reading file.");
     fclose(fp);
     emancipation_proclamation();
     print_time(start, clock());
     print_malloced_mem();
-    print_results("", res);
+    print_results("Positions visited", res);
 }

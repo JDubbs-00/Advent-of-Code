5997821ea8f23edab0406015bbee04df9bb4d6b9
JDubbs-00 <joeljw00@gmail.com>
2024-12-14 20:36:41 -0700

    Day 13a solution

--- 2024/day_13a.c
+++ 2024/day_13a.c
@@ -1,7 +1,7 @@
 #include "../utl.h"
 
-#define TEST
-//#define REAL
+//#define TEST
+#define REAL
 
 // GUESSES:
 
@@ -10,7 +10,8 @@ int main(int argc, char *argv[])
     clock_t start = clock();
     FILE *fp;
     char *line = _malloc(MAX_LINE_LEN);
-    int res;
+    int res, *occurences, tokens, least, press_a, press_b, mul1, mul2, val1, val2;
+    coordinates a, b, prize;
 
     #ifdef TEST
     fp = fopen("input/day_13_example_input.txt", "r");
@@ -24,20 +25,59 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    
+    tokens = 0;
 
     while ((fgets(line, MAX_LINE_LEN, fp)) != NULL)
     {
         clean_input(line);
         //printf("%s", line);
+
+        (void)get_occurences(line, '+', &occurences, 2);
+        a.x = str_to_int(&line[occurences[0] + 1], ',');
+        a.y = str_to_int(&line[occurences[1] + 1], '\0');
+
+        (void)fgets(line, MAX_LINE_LEN, fp);
+        clean_input(line);
+        (void)get_occurences(line, '+', &occurences, 2);
+        b.x = str_to_int(&line[occurences[0] + 1], ',');
+        b.y = str_to_int(&line[occurences[1] + 1], '\0');
+
+        (void)fgets(line, MAX_LINE_LEN, fp);
+        clean_input(line);
+        (void)get_occurences(line, '=', &occurences, 2);
+        prize.x = str_to_int(&line[occurences[0] + 1], ',');
+        prize.y = str_to_int(&line[occurences[1] + 1], '\0');
+
+        least = (int)lcm(a.x, a.y);
+
+        mul1 = least / a.x;
+        a.x *= mul1;
+        b.x *= mul1;
+        prize.x *= mul1;
+
+        mul2 = least / a.y;
+        a.y *= mul2;
+        b.y *= mul2;
+        prize.y *= mul2;
+
+        val1 = prize.x - prize.y;
+        val2 = b.x - b.y;
+        press_b = val1 / val2;
+        if (press_b * val2 == val1)
+        {
+            press_a = (prize.x - (b.x * press_b)) / a.x;
+            tokens += ((3 * press_a) + press_b);
+        }
+
+        (void)fgets(line, MAX_LINE_LEN, fp); // skip empty line
     }
 
-    
+    res = tokens;
 
     if (ferror(fp)) printf("Error reading file.");
     fclose(fp);
     emancipation_proclamation();
     print_time(start, clock());
     print_malloced_mem();
-    print_results("", res);
+    print_results("Fewest tokens", res);
 }
